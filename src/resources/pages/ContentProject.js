fbdb87f1f9da312911bf3b3796be9105a2246c8b
tali <u6980736@anu.edu.au>
2020-07-18 12:45:46 +1000

    gh pages

--- src/resources/pages/ContentProject.js
+++ src/resources/pages/ContentProject.js
@@ -1,56 +1,23 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 import Toolbar from "@material-ui/core/Toolbar";
-import AppBar from '@material-ui/core/AppBar';
+import AppBar from "@material-ui/core/AppBar";
 import Divider from "@material-ui/core/Divider";
-import Button from '@material-ui/core/Button'
+import Button from "@material-ui/core/Button";
 import useTheme from "@material-ui/core/styles/useTheme";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Content() {
-    let theme = useTheme();
-    return (
-        <div>
-            <div>sdfdsfg</div>
-                <Link to={"/about"}>This is a link!</Link>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-            <div>sdfdsfg</div>
-        </div>
-    );
+  let theme = useTheme();
+  return (
+    <div>
+      <div className="bodyParagraph">
+        <p>
+          I've worked on a number of things, here are a few of my favourites.
+        </p>
+      </div>
+    </div>
+  );
 }
